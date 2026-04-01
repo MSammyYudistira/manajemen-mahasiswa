9b096b5bf731f4e6e56ab379671afab1c6b98693
Muhammad Sammy Yudistira <msyudistiraa1@gmail.com>
2026-04-02 02:07:23 +0800

    Fix base URL for Vercel deployments

--- app/config/app.php
+++ app/config/app.php
@@ -4,5 +4,5 @@ declare(strict_types=1);
 
 return [
     'app_name' => 'Manajemen Mahasiswa',
-    'base_url' => getenv('APP_BASE_URL') ?: '/manajemen-mahasiswa',
+    'base_url' => getenv('APP_BASE_URL') ?: (getenv('VERCEL') ? '' : '/manajemen-mahasiswa'),
 ];
